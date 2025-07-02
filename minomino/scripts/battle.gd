d4a2cb35fa9dbdfa80ed07200e42d2b23e8b03d0
yuri.tateo.03 <yuri.tateo@gmail.com>
2025-07-02 22:25:15 +0200

    Fix allo script

--- minomino/scripts/battle.gd
+++ minomino/scripts/battle.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 var timer = 5;
 var was_pressed = false;
-var press_window = 0.2;
+const press_window = 0.2;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,24 +15,16 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if (timer == 0):
-		if ($Timer.is_stopped() == false && was_pressed == false):
-			if Input.is_action_pressed("ui_up"):
-				print(1 - $Timer.time_left);
-				if ((1 - $Timer.time_left) <= press_window):
-					$Label2.text = "Hai ottenuto un bonus!";
-				else:
-					$Label2.text = "Non hai azzeccato il timing!";
-				was_pressed = true;
-	else:
-		if ($Timer.is_stopped() == false && was_pressed == false):
-			if Input.is_action_pressed("ui_up"):
-				print($Timer.time_left);
-				if ($Timer.time_left <= press_window && timer == 1):
-					$Label2.text = "Hai ottenuto un bonus!";
-				else:
-					$Label2.text = "Non hai azzeccato il timing!";
-				was_pressed = true;
+	if ($Timer.is_stopped() == false && was_pressed == false):
+		if Input.is_action_pressed("ui_up"):
+			#Il controllo del timing è effettuato prima e dopo
+			#press_window secondi da quando il timer raggiunge 0
+			if (($Timer.time_left <= press_window && timer == 1) || 
+			   (1 - $Timer.time_left <= press_window && timer == 0)):
+				$Label2.text = "Hai ottenuto un bonus!";
+			else:
+				$Label2.text = "Non hai azzeccato il timing!";
+			was_pressed = true;
 
 func _on_timer_timeout() -> void:
 	timer = timer - 1;
