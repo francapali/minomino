f35250ec7f9b482a11f33b60a9e243b88e02ada9
yuri.tateo.03 <yuri.tateo@gmail.com>
2025-07-03 23:37:30 +0200

    Aggiunta metodo per il check sulla fine del match

--- minomino/scripts/Player.gd
+++ minomino/scripts/Player.gd
@@ -16,7 +16,11 @@ var cur_theater_points: int
 
 # Attributi per la gestione delle mosse nel turno
 var move: Move
+var last_move: Move
 var move_selected_time: float
+
+# Booleano che indica se la mossa speciale può essere eseguita
+var can_make_special_move: bool
 
 # Imposta i parametri del giocatore in base al personaggio che ha selezionato
 func initialize_player(name: String) -> void:
@@ -27,8 +31,48 @@ func initialize_player(name: String) -> void:
 	match p_name:
 		"Teseo": 
 			hp = 50
+			atk = 10
+			def = 10
 			max_theater_points = 3
+			can_make_special_move = true
 		"Minotauro": 
 			hp = 50
+			atk = 10
+			def = 10
 			max_theater_points = 2
+			can_make_special_move = false
 		# Altri personaggi coming soon...
+
+# Effettua la mossa speciale del personaggio.  La mossa speciale varia in base al personaggio scelto.
+# Prende come parametro il nemico e qualche sua info che può essere utile in base all'attacco,
+# oltre a un booleano che indica se il giocatore ha azzeccato il tempismo o meno.
+func special_move(enemy: Player, penalty: bool, dmg_reduction: int) -> void:
+	match p_name:
+		
+		# Mossa speciale di Teseo - Gomitolo: Guadagna un punto teatrale. Se l'avversario
+		# attacca, però, non guadagna un punto e dovrà ricaricare il gomitolo
+		"Teseo":
+			if enemy.last_move == Move.ATTACK:
+				can_make_special_move = false
+			elif not penalty:
+				if can_make_special_move == false:
+					can_make_special_move = true
+				else:
+					cur_theater_points += 1
+				
+			
+		# Mossa speciale del Minotauro - Artigli: Attacco potenziato che fa il 50% dei danni in più,
+		# però deve essere caricata per un turno prima di usarla
+		"Minotauro":
+			if can_make_special_move == false:
+				if not penalty:
+					can_make_special_move = true
+			else:
+				if not penalty:
+					enemy.hp -= (round(atk * 1.5) - dmg_reduction)
+				else:
+					enemy.hp -= (atk - dmg_reduction)
+				can_make_special_move = false
+
+func toString() -> String:
+	return "Nome: %s, HP: %d, Punti teatro: %d" % [p_name, hp, cur_theater_points]
--- minomino/scripts/battle.gd
+++ minomino/scripts/battle.gd
@@ -4,16 +4,19 @@ extends Node2D
 enum Move {NONE, ATTACK, DEFEND, SPECIAL}
 
 # Istanze dei giocatori
-var p1 = Player.new();
-var p2 = Player.new();
+var p1 = Player.new()
+var p2 = Player.new()
 
 # Variabili per la gestione dei turni
-var processing_turn = false;
-var turn_start_time = 0;
-var cur_turn = 0;
+var processing_turn = false
+var turn_start_time = 0
+var cur_turn = 0
 
-const TURN_DURATION = 5;
-const PERFECT_WINDOW = 0.5;
+# Costanti di gioco
+const PENALTY_PERCENTAGE = 0.8
+const TURN_DURATION = 5
+const MAX_TURN = 5
+const PERFECT_WINDOW = 0.9
 
 # Called when the node enters the scene tree for the first time.
 # Inizializza i parametri dei player e fa cominciare la partita.
@@ -21,15 +24,17 @@ func _ready() -> void:
 	p1.initialize_player("Teseo")
 	p2.initialize_player("Minotauro")
 	
-	$Label.position = Vector2(100, 100)
-	$Label2.position = Vector2(300, 100)
+	$Player1.get_child(4).position = Vector2(100, 150)
+	$Player2.get_child(4).position = Vector2(500, 150)
 	
-	$Background.position = Vector2(400, 400)
+	$Label.position = Vector2(100, 100)
+	$Label2.position = Vector2(500, 100)
 	
 	start_turn()
 
 # Chiamata all'inizio di un turno per inizializzare i valori e far partire il timer
 func start_turn() -> void:
+	
 	cur_turn += 1;
 	$Label2.text = "Preparati..."
 	
@@ -44,7 +49,6 @@ func start_turn() -> void:
 
 # Chiamata quando scade il tempo, notifica i giocatori e passa al processing delle mosse
 func _on_timer_timeout() -> void:
-	$Timer.stop()
 	$Label.text = "TAURO!"
 	$Label2.text = "SELEZIONA LA MOSSA ORA!"
 	await get_tree().create_timer(PERFECT_WINDOW).timeout
@@ -54,8 +58,12 @@ func _on_timer_timeout() -> void:
 	await get_tree().create_timer(3).timeout
 	$Label2.text = "Preparati..."
 	process_moves()
-	processing_turn = false
-	$Timer.start()
+	
+	if end_condition():
+		end_match()
+	else:
+		processing_turn = false
+		start_turn();
 
 # Funzione che gestisce e processa gli input dei giocatori durante la partita
 func _unhandled_input(event) -> void:
@@ -66,26 +74,26 @@ func _unhandled_input(event) -> void:
 	
 	if p1.move == Move.NONE:
 		match event.keycode:
-			KEY_D: 
+			KEY_Q: 
 				p1.move = Move.ATTACK
 				p1.move_selected_time = now
-			KEY_S: 
+			KEY_E: 
 				p1.move = Move.DEFEND
 				p1.move_selected_time = now
-			KEY_A: 
+			KEY_W: 
 				p1.move = Move.SPECIAL
 				p1.move_selected_time = now
 		
 		
 	if p2.move == Move.NONE:
 		match event.keycode:
-			KEY_LEFT: 
+			KEY_A: 
 				p2.move = Move.ATTACK
 				p2.move_selected_time = now
-			KEY_DOWN: 
+			KEY_D: 
 				p2.move = Move.DEFEND
 				p2.move_selected_time = now
-			KEY_RIGHT: 
+			KEY_S: 
 				p2.move = Move.SPECIAL
 				p2.move_selected_time = now
 		
@@ -94,14 +102,96 @@ func _unhandled_input(event) -> void:
 # Chiamata alla fine del turno, effettua le mosse selezionate dai player e aggiorna le statistiche
 func process_moves() -> void:
 	var target_time = turn_start_time + TURN_DURATION
-
+	
+	# Calcola la distanza in secondi tra l'input dell'utente e lo scadere del timer
 	var p1_offset = abs(p1.move_selected_time - target_time)
 	var p2_offset = abs(p2.move_selected_time - target_time)
 	
-	print("P1 - Mossa: %d - Offset di tempo: %f" % [p1.move, p1_offset])
-	print("P2 - Mossa: %d - Offset di tempo: %f" % [p2.move, p2_offset])
+	print(p1_offset < PERFECT_WINDOW)
+	print(p2_offset < PERFECT_WINDOW)
 	
+	var p1_dmg_reduction = 0
+	var p2_dmg_reduction = 0
+	
+	#if p1.last_move == p1.move:
+	#if p2.last_move == p2.move:
+	
+	# Se P1 si difende guadagna una riduzione danni in base al tempismo
+	if p1.move == Move.DEFEND:
+		if p1_offset <= PERFECT_WINDOW:
+			p1_dmg_reduction = p1.def
+		else:
+			# Penalità: Riduce la riduzione danni del 20%
+			p1_dmg_reduction = round(p2.def * PENALTY_PERCENTAGE)
+			
+	# Se P2 si difende guadagna una riduzione danni in base al tempismo
+	if p2.move == Move.DEFEND:
+		if p2_offset <= PERFECT_WINDOW:
+			p2_dmg_reduction = p1.def
+		else:
+			# Penalità: Riduce la riduzione danni del 20%
+			p2_dmg_reduction = round(p2.def * PENALTY_PERCENTAGE)
+			
+	# Attacco P1 - Toglie danni a P2 pari all'attacco di P1 meno la riduzione danni di P2
+	if p1.move == Move.ATTACK:
+		if p1_offset <= PERFECT_WINDOW:
+			p2.hp -= (p1.atk - p2_dmg_reduction)
+		else:
+			# Penalità: Riduce il danno inflitto del 20%
+			p2.hp -= (round(p1.atk * 0.8) - p2_dmg_reduction)
+			
+	# Attacco P2 - Toglie danni a P1 pari all'attacco di P2 meno la riduzione danni di P1
+	if p2.move == Move.ATTACK:
+		if p2_offset <= PERFECT_WINDOW:
+			p1.hp -= (p2.atk - p1_dmg_reduction)
+		else:
+			# Penalità: Riduce il danno inflitto del 20%
+			p1.hp -= (round(p2.atk * 0.8) - p1_dmg_reduction)
+			
+	# Per le mosse speciali i check sono fatti nel metodo stesso
+	
+	# Mossa speciale P1 - Sarà gestita dal metodo apposito
+	if p1.move == Move.SPECIAL:
+		p1.special_move(p2, p1_offset > PERFECT_WINDOW, p2_dmg_reduction)
+		
+	# Mossa speciale P2 - Sarà gestita dal metodo apposito
+	if p2.move == Move.SPECIAL:
+		p2.special_move(p1, p2_offset > PERFECT_WINDOW, p1_dmg_reduction)
+	
+	#print("P1 - Mossa: %d - Offset di tempo: %f" % [p1.move, p1_offset])
+	#print("P2 - Mossa: %d - Offset di tempo: %f" % [p2.move, p2_offset])
+
+# Chiamata alla fine di ogni turno, verifica se il match è terminato o meno
+func end_condition() -> bool:
+	# Uno dei due player è arrivato a 0 di vita
+	if p1.hp <= 0 or p2.hp <= 0:
+		return true
+	
+	# Uno dei due player ha raggiunto il numero massimo di punti teatro
+	if p1.cur_theater_points >= p1.max_theater_points or p2.cur_theater_points >= p2.max_theater_points:
+		return true
+	
+	# Sono stati superati i turni massimi del match
+	if cur_turn >= MAX_TURN:
+		return true
+	
+	return false
+
+# Gestisce la fine del match
+func end_match():
+	# Vince il player con più HP
+	if p1.hp > p2.hp:
+		print("Vince " + p1.p_name + "!")
+	elif p2.hp > p1.hp:
+		print("Vince " + p2.p_name + "!")
+	else:
+		print("Abbiamo un pareggio!")
+	
+	# Mettere qui il check sul numero di match e la partenza di un nuovo match
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if not $Timer.is_stopped():
 		$Label.text = str(int($Timer.time_left) + 1)
+		$Player1.get_child(4).text = p1.toString()
+		$Player2.get_child(4).text = p2.toString()
